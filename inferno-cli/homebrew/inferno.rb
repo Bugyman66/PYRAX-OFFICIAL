ff55db46e59d748fb8f1e0cde60eab88fb992e37
GitHub Action <action@github.com>
2026-01-19 07:55:25 +0000

    chore: update Homebrew formula to v0.2.48

--- inferno-cli/homebrew/inferno.rb
+++ inferno-cli/homebrew/inferno.rb
@@ -1,65 +1,30 @@
-# Homebrew formula for Inferno CLI
-# To install: brew tap pyrax-chain/tap && brew install inferno
-
 class Inferno < Formula
   desc "Terminal-based node management for PYRAX blockchain"
   homepage "https://github.com/PYRAX-Chain/PYRAX-OFFICIAL"
-  version "0.2.47"
+  version "0.2.48"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/PYRAX-Chain/PYRAX-OFFICIAL/releases/download/cli-v#{version}/inferno-cli-#{version}-darwin-aarch64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_ARM64"
+      sha256 "0fea0b0e9802b1ffc350c068f9c3c18c8c1a543eb9fe3ce3871c74e1331f2a16"
     end
     on_intel do
       url "https://github.com/PYRAX-Chain/PYRAX-OFFICIAL/releases/download/cli-v#{version}/inferno-cli-#{version}-darwin-x86_64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_X64"
+      sha256 "d5e4d91e596e227acf349264b2c915496dcea9a7235598ef987a1e93f88a7ecb"
     end
   end
 
   on_linux do
-    on_arm do
-      url "https://github.com/PYRAX-Chain/PYRAX-OFFICIAL/releases/download/cli-v#{version}/inferno-cli-#{version}-linux-aarch64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_LINUX_ARM64"
-    end
     on_intel do
       url "https://github.com/PYRAX-Chain/PYRAX-OFFICIAL/releases/download/cli-v#{version}/inferno-cli-#{version}-linux-x86_64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_LINUX_X64"
+      sha256 "690f09ad3fecb825690aaa663bf5b2f2feeded4e5b62cf02cfb8addb684a607b"
     end
   end
 
   def install
     bin.install "inferno"
-    
-    # Generate and install shell completions
     generate_completions_from_executable(bin/"inferno", "completions")
-  end
-
-  def post_install
-    # Create config directory
-    (var/"lib/inferno").mkpath
-  end
-
-  def caveats
-    <<~EOS
-      To get started with Inferno CLI:
-        
-        1. Initialize your first node:
-           inferno init
-        
-        2. Start your node:
-           inferno start
-        
-        3. View logs:
-           inferno logs --follow
-        
-        4. Open dashboard:
-           inferno dashboard --open
-      
-      For more information, visit:
-        https://github.com/PYRAX-Chain/PYRAX-OFFICIAL
-    EOS
   end
 
   test do
